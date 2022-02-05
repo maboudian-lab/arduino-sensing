86cd50929d6bc03051c1e5afce18fc7933f9c43d
Isaac Zakaria <zakaria@berkeley.edu>
2022-02-04 20:26:10 -0800

    Deprecated!

--- python/monitorNoFigaro.py
+++ python/monitorNoFigaro.py
@@ -3,7 +3,7 @@
 Isaac Zakaria
 1 February 2022
 
-Rev: 1 February 2022
+Rev: 3 February 2022
 Updated to monitor time from Arduino instead of from Python.
 Fixed issues where data logging isn't synchronized with Arduino output, causing premature termination.
 Added comments.
@@ -14,8 +14,8 @@ import csv
 import numpy as np
 
 # port = '/dev/ttyACM0' # Debian
-# port = 'COM4' # Windows
-port = '/dev/cu.usbmodem21201' # MacOS
+port = 'COM3' # Windows
+# port = '/dev/cu.usbmodem21201' # MacOS
 
 def figaroMonitor(filename, runtime, port=port, baudrate=115200, figaro=True, bme=680, scd=False):
     """
@@ -49,18 +49,19 @@ def figaroMonitor(filename, runtime, port=port, baudrate=115200, figaro=True, bm
         
         print("CSV initialied with headers")
         print(fieldnames)
+        readoutDict = dict.fromkeys(fieldnames)
         while True:
                 b = arduino.readline() # get serial output
                 s = b.decode() # generate string from serial output
-                if s[0] == "x": # don't start logging until the beginning of a complete chunk of data
+                if len(s) != 0 and s[:2] == "x": # don't start logging until the beginning of a complete chunk of data
                     break
         while t - t0 <= runtime: # note: Arduino reports absolute time in milliseconds
             for k in range(len(fieldnames)):
                 b = arduino.readline()
                 s = b.decode()
                 print(s)
-                if len(s) != 0 and s[0] != "x": # prevents Python from trying to record empty lines in serial output
-                    readoutDict[s[0:2]] = float(s[2:])
+                if len(s) != 0 and s[:2] in fieldnames: # prevents Python from trying to record empty lines in serial output
+                    readoutDict[s[:2]] = float(s[2:])
                 t = readoutDict["t "]
                 if t0 == np.inf:
                     t0 = np.copy(t) # save first absolute time for tracking runtime
@@ -69,4 +70,4 @@ def figaroMonitor(filename, runtime, port=port, baudrate=115200, figaro=True, bm
     
     arduino.close()
     
-figaroMonitor('null.csv', 0.25, port=port, figaro=False, bme=280, scd=False)
+figaroMonitor('null2.csv', 0.25, port=port, figaro=False, bme=680, scd=False)
